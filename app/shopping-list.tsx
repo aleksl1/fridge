561b6e247bacf0e61104241c759afe4da2207ede
aleksslawek <aleksander.slawek@mindconsulting.pl>
2023-05-20 20:29:46 +0200

    Add shopping list item actions

--- app/shopping-list.tsx
+++ app/shopping-list.tsx
@@ -1,12 +1,90 @@
-import { FunctionComponent, useContext } from "react";
-import { ScrollView, StyleSheet } from "react-native";
-import { Divider, List, Text } from "react-native-paper";
+import { FunctionComponent, useContext, useMemo, useState } from "react";
+import { ScrollView, StyleSheet, View, Alert } from "react-native";
+import {
+  Badge,
+  Button,
+  Dialog,
+  Divider,
+  IconButton,
+  List,
+  Portal,
+  Text,
+} from "react-native-paper";
 import AddItemForm from "../src/AddItemForm";
-import { ShoppingListCtx } from "../store/shoppingListCtx";
+import {
+  ShoppingListCtx,
+  ShoppingListItemType,
+} from "../store/shoppingListCtx";
 import { spacing } from "../utils/spacing";
 
+type PressedItemType = ShoppingListItemType & { max: number };
+
 const ShoppingList: FunctionComponent = () => {
-  const { items } = useContext(ShoppingListCtx);
+  const { items, increment, decrement, removeItem, total } =
+    useContext(ShoppingListCtx);
+  const [visible, setVisible] = useState(false);
+  const [pressedItem, setPressedItem] = useState<PressedItemType>({
+    name: "",
+    quantity: 0,
+    max: 0,
+  });
+
+  const showDialog = (item: PressedItemType) => {
+    setPressedItem(item);
+    setVisible(true);
+  };
+
+  const hideDialog = () => setVisible(false);
+
+  const incrementPressed = (item: PressedItemType) => {
+    if (item.max === pressedItem?.quantity) return;
+    setPressedItem((prevState) => {
+      const newState = { ...prevState, quantity: prevState?.quantity + 1 };
+      return newState;
+    });
+  };
+
+  const decrementPressed = (item: PressedItemType) => {
+    if (pressedItem?.quantity === 1) return;
+    setPressedItem((prevState) => {
+      const newState = { ...prevState, quantity: prevState?.quantity - 1 };
+      return newState;
+    });
+  };
+
+  const shoppingList = useMemo(
+    () =>
+      items.map((item) => {
+        return (
+          <List.Item
+            key={item.name}
+            title={item.name}
+            onPress={() => {}}
+            description={`amount: ${item.quantity}`}
+            style={{ paddingEnd: 0 }}
+            right={() => (
+              <View style={{ flexDirection: "row" }}>
+                <IconButton icon="minus" onPress={() => decrement(item)} />
+                <IconButton icon="plus" onPress={() => increment(item)} />
+                <IconButton
+                  icon="delete"
+                  iconColor="red"
+                  onPress={() => {
+                    removeItem(item);
+                  }}
+                />
+                <IconButton
+                  icon="fridge"
+                  iconColor="blue"
+                  onPress={() => showDialog({ ...item, max: item.quantity })}
+                />
+              </View>
+            )}
+          />
+        );
+      }),
+    [items, increment, decrement, removeItem, total]
+  );
   return (
     <ScrollView contentContainerStyle={styles.container}>
       <Text variant="titleMedium">Add new item to list:</Text>
@@ -22,14 +100,45 @@ const ShoppingList: FunctionComponent = () => {
       ))}
       <Divider bold horizontalInset />
       <Text variant="titleMedium">Your shopping list:</Text>
-      {items.map((item) => (
-        <List.Item
-          key={item.name}
-          title={item.name}
-          description={`amount: ${item.quantity}`}
-          left={(props) => <List.Icon {...props} icon="circle" />}
-        />
-      ))}
+      {shoppingList}
+      <Portal>
+        {pressedItem && (
+          <Dialog
+            visible={visible}
+            onDismiss={hideDialog}
+            style={{
+              maxWidth: 400,
+              marginHorizontal: "auto",
+              paddingHorizontal: 50,
+            }}
+          >
+            <Dialog.Content>
+              <View style={{ flexDirection: "row" }}>
+                <IconButton
+                  icon="minus"
+                  onPress={() => decrementPressed(pressedItem)}
+                  style={{ paddingTop: 8 }}
+                />
+                <Badge size={40}>{pressedItem.quantity}</Badge>
+                <IconButton
+                  icon="plus"
+                  onPress={() => incrementPressed(pressedItem)}
+                  style={{ paddingTop: 8 }}
+                />
+              </View>
+            </Dialog.Content>
+            <Dialog.Actions>
+              <Button
+                onPress={hideDialog}
+                mode="contained"
+                style={{ paddingHorizontal: 16 }}
+              >
+                {`Put ${pressedItem.quantity} of ${pressedItem.name} in Your fridge!`}
+              </Button>
+            </Dialog.Actions>
+          </Dialog>
+        )}
+      </Portal>
     </ScrollView>
   );
 };
--- store/shoppingListCtx.tsx
+++ store/shoppingListCtx.tsx
@@ -1,9 +1,20 @@
-import { FunctionComponent, ReactNode, createContext, useState } from "react";
+import {
+  FunctionComponent,
+  ReactNode,
+  createContext,
+  useEffect,
+  useState,
+} from "react";
+
+type ItemAction = (item: ShoppingListItemType) => void;
 
 type ShoppingListCtxType = {
   items: ShoppingListItemType[] | [];
-  addItem: (item: ShoppingListItemType) => void;
-  removeItem: (item: ShoppingListItemType) => void;
+  addItem: ItemAction;
+  removeItem: ItemAction;
+  increment: ItemAction;
+  decrement: ItemAction;
+  total: number;
 };
 
 type ShoppingListProviderProps = {
@@ -17,8 +28,11 @@ export type ShoppingListItemType = {
 
 const defaultValue: ShoppingListCtxType = {
   items: [],
-  addItem: (item: ShoppingListItemType) => {},
-  removeItem: (item: ShoppingListItemType) => {},
+  addItem: (item) => {},
+  removeItem: (item) => {},
+  increment: (item) => {},
+  decrement: (item) => {},
+  total: 0,
 };
 
 export const ShoppingListCtx = createContext<ShoppingListCtxType>(defaultValue);
@@ -27,12 +41,42 @@ const ShoppingListProvider: FunctionComponent<ShoppingListProviderProps> = ({
   children,
 }) => {
   const [items, setItems] = useState<ShoppingListItemType[]>([]);
-  const addItem = (item: ShoppingListItemType) => {
+  const [total, setTotal] = useState<number>(0);
+  const addItem: ItemAction = (item) => {
     setItems((prevState) => [...prevState, item]);
+    calculateTotal();
   };
-  const removeItem = (item: ShoppingListItemType) => {
+  const removeItem: ItemAction = (item) => {
     const newItems = items.filter((i) => i.name !== item.name);
     setItems(newItems);
+    calculateTotal();
+  };
+  const increment: ItemAction = (item) => {
+    const newItems = items;
+    const itemIndex = newItems.findIndex((i) => i.name === item.name);
+    newItems[itemIndex].quantity = newItems[itemIndex].quantity + 1;
+    console.log("incr", newItems, itemIndex);
+    setItems(newItems);
+    calculateTotal();
+  };
+  const decrement: ItemAction = (item) => {
+    let newItems = items;
+    const itemIndex = newItems.findIndex((i) => i.name === item.name);
+    if (newItems[itemIndex].quantity === 1) {
+      newItems = items.filter((i) => i.name !== item.name);
+    } else {
+      newItems[itemIndex].quantity = newItems[itemIndex].quantity - 1;
+      console.log("dec", newItems, itemIndex);
+    }
+    setItems(newItems);
+    calculateTotal();
+  };
+  const calculateTotal = () => {
+    const totalQuantity = items.reduce(
+      (total, item) => total + item.quantity,
+      0
+    );
+    setTotal(totalQuantity);
   };
   return (
     <ShoppingListCtx.Provider
@@ -40,6 +84,9 @@ const ShoppingListProvider: FunctionComponent<ShoppingListProviderProps> = ({
         items,
         addItem,
         removeItem,
+        increment,
+        decrement,
+        total,
       }}
     >
       {children}
