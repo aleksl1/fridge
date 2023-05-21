5f87a4b0d5bcafb35e8ecb14a4058cf24dc2a6d6
aleksslawek <aleksander.slawek@mindconsulting.pl>
2023-05-21 11:44:04 +0200

    Add bottom tabs

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,21 +1,119 @@
 import { FunctionComponent } from "react";
-import { Stack } from "expo-router";
+import { Stack, Tabs } from "expo-router";
 import { View, Platform, StyleSheet } from "react-native";
+import { Text } from "react-native-paper";
 import AppWrapper from "../src/AppWrapper";
+import Icon from "react-native-vector-icons/MaterialCommunityIcons";
 interface LayoutProps {}
+
+const iconSize = 36;
 
 const Layout: FunctionComponent<LayoutProps> = () => {
   if (Platform.OS === "web")
     return (
       <AppWrapper>
         <View style={styles.webContainer}>
-          <Stack />
+          {/* <Stack /> */}
+          <Tabs
+            screenOptions={{
+              tabBarStyle: {
+                height: 100,
+                paddingBottom: 16,
+              },
+            }}
+          >
+            <Tabs.Screen
+              name="index"
+              options={{
+                // This tab will no longer show up in the tab bar.
+                href: null,
+              }}
+            />
+            <Tabs.Screen
+              name="shopping-list"
+              options={{
+                tabBarLabel: "Shopping list",
+                tabBarIcon: ({ color }) => {
+                  return (
+                    <Icon name="clipboard-list" size={iconSize} color={color} />
+                  );
+                },
+              }}
+            />
+            <Tabs.Screen
+              name="fridge"
+              options={{
+                tabBarLabel: "Fridge",
+                tabBarIcon: ({ color }) => {
+                  return <Icon name="fridge" size={iconSize} color={color} />;
+                },
+              }}
+            />
+            <Tabs.Screen
+              name="food-diary"
+              options={{
+                tabBarLabel: "Food Diary",
+                tabBarIcon: ({ color }) => {
+                  return <Icon name="food" size={iconSize} color={color} />;
+                },
+              }}
+            />
+          </Tabs>
         </View>
       </AppWrapper>
     );
   return (
     <AppWrapper>
-      <Stack />
+      <>
+        {/* <Stack /> */}
+        <Tabs
+          screenOptions={{
+            tabBarStyle: {
+              height: 100,
+              paddingBottom: 16,
+            },
+          }}
+        >
+          <Tabs.Screen
+            name="index"
+            options={{
+              // This tab will no longer show up in the tab bar.
+              href: null,
+            }}
+          />
+          <Tabs.Screen
+            name="shopping-list"
+            options={{
+              tabBarLabel: () => (
+                <Text variant="labelLarge">Shopping List</Text>
+              ),
+              tabBarIcon: ({ color }) => {
+                return (
+                  <Icon name="clipboard-list" size={iconSize} color={color} />
+                );
+              },
+            }}
+          />
+          <Tabs.Screen
+            name="fridge"
+            options={{
+              tabBarLabel: () => <Text variant="labelLarge">Fridge</Text>,
+              tabBarIcon: ({ color }) => {
+                return <Icon name="fridge" size={iconSize} color={color} />;
+              },
+            }}
+          />
+          <Tabs.Screen
+            name="food-diary"
+            options={{
+              tabBarLabel: () => <Text variant="labelLarge">Food Diary</Text>,
+              tabBarIcon: ({ color }) => {
+                return <Icon name="food" size={iconSize} color={color} />;
+              },
+            }}
+          />
+        </Tabs>
+      </>
     </AppWrapper>
   );
 };
