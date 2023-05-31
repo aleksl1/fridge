1ce69cf9b206826390f0ba5e07200e3af0d62da1
aleksslawek <aleksander.slawek@mindconsulting.pl>
2023-05-31 17:41:48 +0200

    wrap app in query provider

--- src/AppWrapper.tsx
+++ src/AppWrapper.tsx
@@ -2,6 +2,9 @@ import { FunctionComponent, ReactElement } from "react";
 import { PaperProvider } from "react-native-paper";
 import ItemListProvider from "../store/ItemListCtx";
 import { theme } from "../utils/theme";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 type AppWrapperProps = {
   children: ReactElement;
@@ -9,9 +12,11 @@ type AppWrapperProps = {
 
 const AppWrapper: FunctionComponent<AppWrapperProps> = ({ children }) => {
   return (
-    <ItemListProvider>
-      <PaperProvider theme={theme}>{children}</PaperProvider>
-    </ItemListProvider>
+    <QueryClientProvider client={queryClient}>
+      <ItemListProvider>
+        <PaperProvider theme={theme}>{children}</PaperProvider>
+      </ItemListProvider>{" "}
+    </QueryClientProvider>
   );
 };
 
