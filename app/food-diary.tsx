d1d9f1ca2ca082df25b8eec4ee972dd9ca1599ea
aleksslawek <aleksander.slawek@mindconsulting.pl>
2023-06-03 23:44:35 +0200

    Add different list in food diary Add food diary list item Start working on total calories

--- app/food-diary.tsx
+++ app/food-diary.tsx
@@ -1,14 +1,26 @@
-import { FunctionComponent } from "react";
-import { ScrollView } from "react-native";
+import {FunctionComponent} from "react";
+import {ScrollView} from "react-native";
 import ItemList from "../src/ItemsList";
 import globalStyles from "../utils/globalStyles";
+import {DataTable} from "react-native-paper";
+import {spacing} from "../utils/spacing";
 
 const FoodDiary: FunctionComponent = () => {
-  return (
-    <ScrollView contentContainerStyle={globalStyles.listContainer}>
-      <ItemList type="foodDiary" />
-    </ScrollView>
-  );
+    return (
+        <ScrollView contentContainerStyle={globalStyles.listContainer}>
+            <DataTable style={{paddingEnd: spacing.spacing16}}>
+                <DataTable.Header>
+                    <DataTable.Title style={{flex: 2}}>Product name</DataTable.Title>
+                    <DataTable.Title numeric>Cal</DataTable.Title>
+                    <DataTable.Title numeric>Fats</DataTable.Title>
+                    <DataTable.Title numeric>Prot</DataTable.Title>
+                    <DataTable.Title numeric>Carb</DataTable.Title>
+                </DataTable.Header>
+                <ItemList type="foodDiary"/>
+            </DataTable>
+
+        </ScrollView>
+    );
 };
 
 export default FoodDiary;
--- src/CustomListItem.tsx
+++ src/CustomListItem.tsx
@@ -1,10 +1,10 @@
-import {FunctionComponent, useState} from "react";
-import {Divider, IconButton, List, Menu, Text, useTheme} from "react-native-paper";
+import {FunctionComponent} from "react";
+import {List, useTheme} from "react-native-paper";
 import {ListItemType} from "../store/ItemList.types";
+import {ListItemActionsMenu} from "./ListItemActionsMenu";
 import {View} from "react-native";
-import {spacing} from "../utils/spacing";
 
-type CustomListItemProps = {
+export type CustomListItemProps = {
     item: ListItemType;
     onItemPress: () => void;
     onPlusPress?: () => void;
@@ -14,88 +14,32 @@ type CustomListItemProps = {
     onEditPress?: () => void;
 };
 
-const CustomListItem: FunctionComponent<CustomListItemProps> = ({
-                                                                    item,
-                                                                    onAddToNextListPress,
-                                                                    onDeletePress,
-                                                                    onItemPress,
-                                                                    onMinusPress,
-                                                                    onPlusPress,
-                                                                    onEditPress,
-                                                                }) => {
+const CustomListItem: FunctionComponent<CustomListItemProps> = (props) => {
     const {
-        colors: {error, primary},
+        item,
+        onItemPress,
+    } = props;
+    const {
+        colors: {primary, primaryContainer},
     } = useTheme();
-    const [menuVisible, setMenuVisible] = useState(false);
-    const openMenu = () => setMenuVisible(true);
-    const closeMenu = () => setMenuVisible(false);
+
     return (
         <List.Item
             title={item.name}
             onPress={onItemPress}
             description={`amount: ${item.quantity}`}
-            style={{paddingEnd: 0}}
-            right={() => {
-                const titleStyle = {paddingStart: spacing.spacing16};
-                return (
-                    <View style={{flexDirection: "row"}}>
-                        {item.status === "shoppingList" && (
-                            <IconButton
-                                icon="fridge"
-                                iconColor={primary}
-                                onPress={onAddToNextListPress}
-                            />
-                        )}
-                        {item.status === "fridge" && (
-                            <IconButton
-                                icon="food"
-                                iconColor={primary}
-                                onPress={onAddToNextListPress}
-                            />
-                        )}
-                        <Menu
-                            visible={menuVisible}
-                            onDismiss={closeMenu}
-                            anchor={<IconButton icon="dots-vertical" onPress={openMenu}/>}
-                        >
-                            {onPlusPress && (
-                                <Menu.Item
-                                    titleStyle={titleStyle}
-                                    onPress={onPlusPress}
-                                    title={<Text variant="labelLarge">ADD</Text>}
-                                    leadingIcon={() => <IconButton icon="plus"/>}
-                                />
-                            )}
-                            {onMinusPress && (
-                                <Menu.Item
-                                    titleStyle={titleStyle}
-                                    onPress={onMinusPress}
-                                    title={<Text variant="labelLarge">REMOVE</Text>}
-                                    leadingIcon={() => <IconButton icon="minus"/>}
-                                />
-                            )}
-                            {!onEditPress && ( //todo change condition when edit function is added
-                                <Menu.Item
-                                    titleStyle={titleStyle}
-                                    onPress={() => {
-                                    }}
-                                    title={<Text variant="labelLarge">EDIT</Text>}
-                                    leadingIcon={() => <IconButton icon="pencil"/>}
-                                />
-                            )}
-                            <Divider bold/>
-                            {onDeletePress && (
-                                <Menu.Item
-                                    titleStyle={titleStyle}
-                                    onPress={onDeletePress}
-                                    title={<Text variant="labelLarge">DELETE</Text>}
-                                    leadingIcon={() => <IconButton icon="delete" iconColor={error}/>}
-                                />
-                            )}
-                        </Menu>
-                    </View>
-                );
+            style={{
+                paddingEnd: 0,
+                backgroundColor: primaryContainer,
+                padding: 0,
+                margin: 0,
+                borderColor: primary,
+                borderWidth: 1,
+                borderRadius: 15,
             }}
+            right={() => <View style={{flexDirection: "row"}}>
+                <ListItemActionsMenu {...props}/>
+            </View>}
         />
     );
 };
