e6ece860d0c4ce7bc705acc5b143cf58570c5f77
aleksslawek <aleksander.slawek@mindconsulting.pl>
2023-06-06 20:03:53 +0200

    Change  font color Remove unused colors

--- app/index.tsx
+++ app/index.tsx
@@ -1,18 +1,16 @@
-import { Redirect } from "expo-router";
-import { FunctionComponent } from "react";
-import { StyleSheet, View } from "react-native";
+import {Redirect} from "expo-router";
+import {FunctionComponent} from "react";
+import {View} from "react-native";
 import AppWrapper from "../src/AppWrapper";
-import { colors } from "../utils/colors";
-import { spacing } from "../utils/spacing";
 
 const Home: FunctionComponent = () => {
-  return (
-    <AppWrapper>
-      <View>
-        <Redirect href="/shopping-list" />
-      </View>
-    </AppWrapper>
-  );
+    return (
+        <AppWrapper>
+            <View>
+                <Redirect href="/shopping-list"/>
+            </View>
+        </AppWrapper>
+    );
 };
 
 export default Home;
