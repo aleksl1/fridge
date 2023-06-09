8c6ed18d07edf6083f338aa0ca30dc531ff9f9e4
aleksslawek <aleksander.slawek@mindconsulting.pl>
2023-06-09 21:14:19 +0200

    Add reanimated and its dependency for web

--- babel.config.js
+++ babel.config.js
@@ -2,7 +2,11 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ["babel-preset-expo"],
-    plugins: [require.resolve("expo-router/babel")],
+    plugins: [
+      require.resolve("expo-router/babel"),
+      "@babel/plugin-transform-export-namespace-from",
+      "react-native-reanimated/plugin",
+    ], //reanimated plugin has to be last
     env: {
       production: {
         plugins: ["react-native-paper/babel"],
