bf5e4f69731c38ea80ded1b4769ee4b9edc6952b
aleksslawek <aleksander.slawek@mindconsulting.pl>
2023-07-08 18:51:42 +0200

    test1

---
+++ src/components/TestTest.tsx
@@ -0,0 +1,15 @@
+import { FC } from "react";
+import { View } from "react-native";
+import { Text } from "react-native-paper";
+
+type TestTestProps = {};
+
+const TestTest: FC<TestTestProps> = (props) => {
+  return (
+    <View>
+      <Text>TestTest</Text>
+    </View>
+  );
+};
+
+export default TestTest;
